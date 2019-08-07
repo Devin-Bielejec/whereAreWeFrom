5c54650553772dd7e72929a53812cfbaf6682cc9
devin-bielejec <devin.bieleje@gmail.com>
2019-08-07 14:04:45 -0400

    set up example with get axios request

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,14 @@ const NY = document.querySelector("#NY");
 
 NY.addEventListener("click", e => e.target.style.fill = "green");
 console.log(NY);
+
+const userNames = ["Devin-Bielejec", "AceMouty", "rleslie1015", "ajkizer"];
+
+userNames.forEach( item => {
+    axios.get(`https://api.github.com/users/${item}`)
+    .then( response => {
+        console.log(response.data.location);
+        })
+    .catch( err => err)
+}
+)
